384a114e6ea929ab7ea57e3861073fb0347d8dba
Gunczlinger <rgunczl1@binghamton.edu>
2019-10-07 10:40:02 -0400

    back on the horse
    also spent entire morning deleting lock files from buggy emulator

--- app/src/main/java/com/example/converter/MainActivity.java
+++ app/src/main/java/com/example/converter/MainActivity.java
@@ -18,6 +18,13 @@ import android.widget.ArrayAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
+    public Spinner classSpinner;
+    public Spinner inSpinner;
+    public Spinner outSpinner;
+    public ArrayAdapter<CharSequence> classAdapter;
+    public ArrayAdapter<CharSequence> tempAdapter;
+    public ArrayAdapter<CharSequence> distAdapter;
+    public ArrayAdapter<CharSequence> weightAdapter;
 
 
     @Override
@@ -28,29 +35,29 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         //Start Spinner code
-        Spinner classSpinner = (Spinner) findViewById(R.id.convert_class);
+        classSpinner = (Spinner) findViewById(R.id.convert_class);
         // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> classAdapter = ArrayAdapter.createFromResource(this,
+        classAdapter = ArrayAdapter.createFromResource(this,
                 R.array.conversion_classes, android.R.layout.simple_spinner_item);
         // Specify the layout to use when the list of choices appears
         classAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
         classSpinner.setAdapter(classAdapter);
 
-        ArrayAdapter<CharSequence> tempAdapter = ArrayAdapter.createFromResource(this,
+        tempAdapter = ArrayAdapter.createFromResource(this,
                R.array.temp_conversion, android.R.layout.simple_spinner_item);
         tempAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        ArrayAdapter<CharSequence> distAdapter = ArrayAdapter.createFromResource(this,
+        distAdapter = ArrayAdapter.createFromResource(this,
                R.array.dist_conversion, android.R.layout.simple_spinner_item);
         distAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        ArrayAdapter<CharSequence> weightAdapter = ArrayAdapter.createFromResource(this,
+        weightAdapter = ArrayAdapter.createFromResource(this,
                R.array.weight_conversion, android.R.layout.simple_spinner_item);
         weightAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        Spinner inSpinner = (Spinner) findViewById(R.id.in_spinner);
+        inSpinner = (Spinner) findViewById(R.id.in_spinner);
         inSpinner.setAdapter(tempAdapter);
 
-        Spinner outSpinner = (Spinner) findViewById(R.id.out_spinner);
+        outSpinner = (Spinner) findViewById(R.id.out_spinner);
         outSpinner.setAdapter(tempAdapter);
 
 
@@ -89,15 +96,28 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void setOptions(string opt)
-        switch(opt){
-        case "t":
-            break;
-        case "d":
-            break;
-        case "w":
-            break;
-        default:
-            ;
+    private void setOptions(String opt) {
+        switch (opt) {
+            case "t":
+                inSpinner.setAdapter(tempAdapter);
+                outSpinner.setAdapter(tempAdapter);
+                break;
+            case "d":
+                inSpinner.setAdapter(distAdapter);
+                outSpinner.setAdapter(distAdapter);
+                break;
+            case "w":
+                inSpinner.setAdapter(weightAdapter);
+                outSpinner.setAdapter(weightAdapter);
+                break;
+            default:
+                inSpinner.setAdapter(weightAdapter);
+                outSpinner.setAdapter(weightAdapter);
+                ;
         }
+    }
+
+    public float convert(){
+        return 1;
+    }
 }
